6f5d953da39727246dfdf29ae14545676ee7518a
SinchanaUrs1101 <sinchuurs17@gmail.com>
2025-10-06 07:07:03 +0000

    variable pushed

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 14453 
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+let accountState
+
+//accountId = 2
+accountEmail = "surs@gg.com"
+accountPassword = "1101"
+accountCity = "Pune"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
