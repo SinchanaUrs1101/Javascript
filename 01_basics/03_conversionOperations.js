895907225431f90f331c74929363290b74657ae6
SinchanaUrs1101 <sinchuurs17@gmail.com>
2025-10-07 11:45:14 +0000

    conversion committed

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "Sinchu"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN when you try  to convert the 33abc to number it takes the input as NotaNumber
+// true => 1, false => 0
+//under=fined => NaN
+//null => 0
+let isLoggedIn = "Urs"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0=>false
+// "" => false; "sinchu"=>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
